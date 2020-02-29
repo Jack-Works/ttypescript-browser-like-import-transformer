8e563a683de302241ab27acbec3f6b411fa37f94
Jack Works <jackworks@protonmail.com>
2020-03-01 00:01:36 +0800

    chore: bump version for tests

--- specs/__snapshot__/demo-4.js
+++ specs/__snapshot__/demo-4.js
@@ -5,4 +5,4 @@ __customImportHelper('react' + x, __customDynamicImportHelper(__dynamicImportTra
 function __dynamicImportNative(path) {
     return import(path);
 }
-import { __dynamicImportTransform as __dynamicImportTransform, __UMDBindCheck as __UMDBindCheck, moduleSpecifierTransform as moduleSpecifierTransform, __customDynamicImportHelper as __customDynamicImportHelper } from "https://cdn.jsdelivr.net/npm/@magic-works/ttypescript-browser-like-import-transformer@1.5.0/es/ttsclib.min.js";
+import { __dynamicImportTransform as __dynamicImportTransform, __UMDBindCheck as __UMDBindCheck, moduleSpecifierTransform as moduleSpecifierTransform, __customDynamicImportHelper as __customDynamicImportHelper } from "https://cdn.jsdelivr.net/npm/@magic-works/ttypescript-browser-like-import-transformer@2.0.0/es/ttsclib.min.js";
