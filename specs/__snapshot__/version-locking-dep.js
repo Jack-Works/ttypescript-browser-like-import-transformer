7249ecf14f6d2b850fa9771e440a9bee4834b6b9
Jack Works <jackworks@protonmail.com>
2020-10-05 10:49:16 +0800

    feat!: minimal ts version require 4.1

--- specs/__snapshot__/version-locking-dep.js
+++ specs/__snapshot__/version-locking-dep.js
@@ -1,4 +1,4 @@
 // CompilerOptions: {"module":"ESNext"}
 // PluginConfig: {"rules":"pikacdn"}
-import * as typescript from "https://cdn.skypack.dev/typescript@4.0.3";
+import * as typescript from "https://cdn.skypack.dev/typescript@4.1.0-dev.20201004";
 console.log(typescript);
